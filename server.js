94fbb28b26c3e8341ee4459e7f8f90cf896b2308
abhishekbain220 <you@example.co>
2025-07-11 15:12:38 +0530

    1

---
+++ server.js
@@ -0,0 +1,51 @@
+require("dotenv").config()
+let db=require("./model/connect")
+let express=require("express")
+let cookieParser=require("cookie-parser")
+let morgan=require("morgan")
+let cors=require("cors")
+const { errorHandler } = require("./middleware/errorHandler")
+let PORT=process.env.PORT || 3000
+let app=express()
+let userRouter=require("./Routes/userRouter")
+let taskRouter=require("./Routes/taskRouter")
+
+
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(morgan("tiny"))
+
+const allowedOrigins = [
+  "http://localhost:5173",             // for local development
+  "https://reminderf.vercel.app",  // replace with your real frontend domain
+];
+
+app.use(cors({
+    origin:allowedOrigins,
+    credentials:true
+}))
+
+app.use((req,res)=>{
+    res.send("Backend is running")
+})
+
+// Routes
+app.use("/user",userRouter)
+app.use("/task",taskRouter)
+
+
+
+app.use((req,res,next)=>{
+    res.status(404).json({
+        success:false,
+        message:"Route not Found"
+    })
+})
+
+app.use(errorHandler)
+
+app.listen(PORT,()=>{
+    console.log("Server running on PORT",PORT)
+})
